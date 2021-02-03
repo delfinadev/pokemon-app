58501fe1f2043ab53afc3dec1f32431281f6a69c
Federico Tanco”git config --global user.email fedetanco84@gmail.comgit config --global user.name Federico <fedetanco84@gmail.com>
2021-02-03 13:32:05 -0300

    commiArchivose

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import "./App.css";
+import SearchInput from "./components/SearchInput";
+import axios from "axios";
 
 function App() {
+  const [data, setData] = useState([]);
+  const [pokemon, setPokemon] = useState([]);
+
+  useEffect(() => {
+    let url = ` https://pokeapi.co/api/v2/pokemon/${pokemon}`;
+    axios.get(url).then((response) => {
+      setData(response.data.results);
+    });
+  }, [pokemon]);
+
+  function getPokemon(poke) {
+    setPokemon(poke);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Pokemon app</h1>
+      <SearchInput getPokemon={getPokemon} />
+      {/* <Cards data={data}/> */}
     </div>
   );
 }
---
+++ src/components/SearchInput.js
@@ -0,0 +1,28 @@
+import React, { useState } from "react";
+
+export default function SearchInput({ getPokemon }) {
+  const [search, setSearch] = useState("");
+
+  function handleChange(e) {
+    setSearch(e.target.value);
+  }
+
+  return (
+    <>
+      <form>
+        <label>Busca tu Pokemon preferido</label>
+        <input
+          type="text"
+          name="pokemon"
+          className=""
+          placeholder="Busca tu Pokemon"
+          onChange={handleChange}
+          // value={pokemon}
+        />
+      </form>
+      <button className="btn-danger " onClick={(e) => getPokemon(search)}>
+        Buscar
+      </button>
+    </>
+  );
+}
