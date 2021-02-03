653eacb741b312d7e07d0591fc78fbe4a0614cf1
delfinadev <delfinalasserre@gmail.com>
2021-02-03 16:22:42 -0300

    html cards

---
+++ src/components/Cards.js
@@ -0,0 +1,24 @@
+import React,{}from "react";
+
+
+export default function Cards({ getData }) {
+
+
+  return (
+    <>
+      <div className="pokecard">
+        <img src="" alt=""></img>
+        <div className="card-data">
+          <p className="card-texts">Name: Charmander</p>
+          <p className="card-texts">HP: 39</p>
+          <p className="card-texts">Attack: 52 </p>
+          <p className="card-texts">Defense: 43</p>
+          <p className="card-texts">Type: Fire</p>
+
+        </div>
+
+      </div>
+
+    </>
+  );
+}
