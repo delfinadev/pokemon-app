0d7af39a43313d8344d83bac9a6326f024088bdf
Federico Tanco”git config --global user.email fedetanco84@gmail.comgit config --global user.name Federico <fedetanco84@gmail.com>
2021-02-03 22:47:24 -0300

    lowercase

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,25 +1,35 @@
-import './cards.css'
+import "./cards.css";
 export default function Cards({ data }) {
- 
-
   return (
     <>
-    { data.sprites === undefined ? 
+      {data.sprites === undefined ? (
         <div></div>
-
-      : <div className="pokecard">
-       <img src={data.sprites.other.dream_world.front_default} alt=""></img>  
-        <div className="card-data">
-          <p className="card-texts">Name: {data.name}</p>
-           <p className="card-texts">HP: {data.stats[0].base_stat}</p>
-          <p className="card-texts">Attack: {data.stats[1].base_stat} </p>
-          <p className="card-texts">Defense:{data.stats[2].base_stat}</p>
-          <p className="card-texts">Type: {data.types[0].type.name}</p>  
-
+      ) : (
+        <div className="pokecard">
+          <img src={data.sprites.other.dream_world.front_default} alt=""></img>
+          <div className="card-data">
+            <p className="card-texts">
+              Name: <span className="card-texts-data">{data.name}</span>
+            </p>
+            <p className="card-texts">
+              HP:{" "}
+              <span className="card-texts-data">{data.stats[0].base_stat}</span>
+            </p>
+            <p className="card-texts">
+              Attack:{" "}
+              <span className="card-texts-data">{data.stats[1].base_stat}</span>{" "}
+            </p>
+            <p className="card-texts">
+              Defense:
+              <span className="card-texts-data">{data.stats[2].base_stat}</span>
+            </p>
+            <p className="card-texts">
+              Type:{" "}
+              <span className="card-texts-data">{data.types[0].type.name}</span>
+            </p>
+          </div>
         </div>
-
-      </div>
-}  
+      )}
     </>
   );
 }
